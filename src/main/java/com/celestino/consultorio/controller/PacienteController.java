8c4b44d347a2300472ece44d1d27d88f3fc46ecc
MairaCelestino <mairacelestino@hotmail.com>
2020-08-12 20:52:50 +0100

    includes deleteById method + DELETE postman

--- src/main/java/com/celestino/consultorio/controller/PacienteController.java
+++ src/main/java/com/celestino/consultorio/controller/PacienteController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +42,14 @@ public class PacienteController {
 
 		}
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable Long id) {
+		Paciente pacienteDeletado = pacienteRepository.deleteById(id);
+		if (pacienteDeletado != null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<>(pacienteDeletado, HttpStatus.OK);
 	}
 }
